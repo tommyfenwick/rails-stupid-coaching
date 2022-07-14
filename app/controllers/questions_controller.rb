7acf641abd6787c8c56dda7d105a76f8cd819bca
Tommy Fenwick <tommyfenwick9@hotmail.com>
2022-07-14 20:25:41 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @user_input = params[:question]
+    if @user_input == "I am going to work"
+      @coach_answer = 'Great!'
+
+    elsif @user_input.end_with?('?')
+      @coach_answer = 'Silly question, get dressed and go to work!'
+
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
